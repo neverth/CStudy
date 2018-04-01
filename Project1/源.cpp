1a169fef876bdf7d3af196b5c7cd3930163c118c
NEVERTHELESS0 <36693967+NEVERTHELESS0@users.noreply.github.com>
2018-04-01 22:58:39 +0800

    文本文件练习

---
+++ Project1/源.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//int set(int t_x, int t_y) {
+//	/*int x, int y;
+//	x = t_x;
+//	y = t_y;*/
+//	return (t_x, t_y);
+//}
+//int main() {
+//	cout << set(88, 99) << endl;
+//	system("pause");
+//	return 0;
+//}
