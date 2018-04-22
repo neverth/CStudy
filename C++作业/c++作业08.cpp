a9070f605a8135ed9cf3736264a1dd8ac09627fa
NEVERTHELESS0 <36693967+NEVERTHELESS0@users.noreply.github.com>
2018-04-22 11:13:12 +0800

    增加作业8， 9

---
+++ C++作业/c++作业08.cpp
@@ -0,0 +1,125 @@
+﻿/*
+*C++作业08
+*设计一个计数器类Calculator，它只有一个用于计数的数据成员count。
+*实现两个计数器的加减法乘除四则运算。（分别用成员函数和友元函数实现）
+*2018/04/22
+*/
+//常规解法
+#include <iostream>
+using namespace std;
+class Calculator {
+private:
+	float count;
+public:
+	Calculator(float t_count):count(t_count) {}
+	void show() {
+		cout << "count = " << count << endl;
+	}
+	//成员函数实现
+	Calculator add_mFuntion(const Calculator &t_count) {
+		return Calculator(this->count + t_count.count);
+	}
+	Calculator sub_mFuntion(const Calculator &t_count) {
+		return Calculator(this->count - t_count.count);
+	}
+	Calculator mul_mFuntion(const Calculator &t_count) {
+		return Calculator(this->count * t_count.count);
+	}
+	Calculator div_mFuntion(const Calculator &t_count) {
+		return Calculator(this->count / t_count.count);
+	}
+	//友元函数实现
+	friend Calculator add_fFuntion(const Calculator &t_count1, const Calculator &t_count2);
+	friend Calculator sub_fFuntion(const Calculator &t_count1, const Calculator &t_count2);
+	friend Calculator mul_fFuntion(const Calculator &t_count1, const Calculator &t_count2);
+	friend Calculator div_fFuntion(const Calculator &t_count1, const Calculator &t_count2);
+};
+Calculator add_fFuntion(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count + t_count2.count);
+}
+Calculator sub_fFuntion(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count - t_count2.count);
+}
+Calculator mul_fFuntion(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count * t_count2.count);
+}
+Calculator div_fFuntion(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count / t_count2.count);
+}
+int main() {
+	Calculator a(100.99), b(200.01);
+	cout << "成员函数调用" << endl;
+	a.add_mFuntion(b).show();
+	a.sub_mFuntion(b).show();
+	a.mul_mFuntion(b).show();
+	a.div_mFuntion(b).show();
+	cout << "友元函数调用" << endl;
+	add_fFuntion(a, b).show();
+	sub_fFuntion(a, b).show();
+	mul_fFuntion(a, b).show();
+	div_fFuntion(a, b).show();
+	system("pause");
+	return 0;
+}
+
+
+
+/*
+*使用运算符重载,可以对比出没有使用运算符重载的区别
+*/
+#include <iostream>
+using namespace std;
+class Calculator {
+private:
+	float count;
+public:
+	Calculator(float t_count):count(t_count) {}
+	void show() {
+		cout << "count = " << count << endl;
+	}
+	//成员函数实现
+	Calculator operator+(const Calculator &t_count) {
+		return Calculator(this->count + t_count.count);
+	}
+	Calculator operator-(const Calculator &t_count) {
+		return Calculator(this->count - t_count.count);
+	}
+	Calculator operator*(const Calculator &t_count) {
+		return Calculator(this->count * t_count.count);
+	}
+	Calculator operator/(const Calculator &t_count) {
+		return Calculator(this->count / t_count.count);
+	}
+	//友元函数实现
+	friend Calculator operator+(const Calculator &t_count1, const Calculator &t_count2);
+	friend Calculator operator-(const Calculator &t_count1, const Calculator &t_count2);
+	friend Calculator operator*(const Calculator &t_count1, const Calculator &t_count2);
+	friend Calculator operator/(const Calculator &t_count1, const Calculator &t_count2);
+};
+Calculator operator+(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count + t_count2.count);
+}
+Calculator operator-(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count - t_count2.count);
+}
+Calculator operator*(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count * t_count2.count);
+}
+Calculator operator/(const Calculator &t_count1, const Calculator &t_count2) {
+	return Calculator(t_count1.count / t_count2.count);
+}
+int main() {
+	Calculator a(100.99), b(200.01);
+	cout << "成员函数调用" << endl;
+	(a + b).show();
+	(a - b).show();
+	(a * b).show();
+	(a / b).show();
+	cout << "友元函数调用" << endl;
+	(a + b).show();
+	(a - b).show();
+	(a * b).show();
+	(a / b).show();
+	system("pause");
+	return 0;
+}
---
+++ C++作业/c++作业09.cpp
@@ -0,0 +1,50 @@
+﻿/*
+*C++作业09
+*定义一个point类，包含私有数据成员x，y，成员函数包括无参构造函数，带参构造函数，
+*set和get属性函数。定义circle类，从point类公有派生，增加数据成员半径r，
+*成员函数包括无参构造函数，带参构造函数，计算面积函数getarea。
+*在main函数中定义一个circle的对象，并计算其面积。
+*2018/04/22
+*/
+#include <iostream>
+using namespace std;
+const float PI = 3.1415926;
+class Point {
+private:
+	int x, y;
+public:
+	Point() {}
+	Point(int t_x, int t_y):x(t_x), y(t_y){}
+	void set_x(const int &t_x) {
+		x = t_x;
+	}
+	void set_y(const int &t_y) {
+		x = t_y;
+	}
+	int get_x() { 
+		return x;
+	}
+	int get_y() {
+		return y;
+	}
+};
+class Circle :public Point {
+private:
+	float r;
+public:
+	Circle() {}
+	Circle(int t_x, int t_y, float t_r) :Point(t_x, t_y), r(t_r) {}
+	float get_area() {
+		return r * r * PI;
+	}
+};
+int main() {
+	Circle a(88, 99, 18);
+	cout << "面积为： " << a.get_area() << endl;
+	cout << "其坐标为：（" << a.get_x() << ", " << a.get_y() << ")" << endl;
+	a.set_x(55);
+	a.set_y(66);
+	cout << "更改之后其坐标为：（" << a.get_x() << ", " << a.get_y() << ")" << endl;
+	system("pause");
+	return 0;
+}
