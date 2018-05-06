f11dae2886d7acdd5342658353541539d27d4317
NEVERTHELESS0 <36693967+NEVERTHELESS0@users.noreply.github.com>
2018-05-06 20:32:00 +0800

    上传作业09/10

---
+++ C++作业/c++作业10.cpp
@@ -0,0 +1,54 @@
+﻿/*先建立一个Point（点）类，包含数据成员x，y（坐标点）。
+以它为基类，派生出一个Circle（圆）类，增加数据成员r（半径），
+再以Circle类为直接基类，派生出一个Cylinder（圆柱体）类，
+在增加数据成员h（高）。各类中都有一个cal函数
+（其中point中为虚函数，circle中为求面积，cylinder中为求体积），
+通过使用point类的指针来指向不同对象，来输出不同结果*/
+
+#include<iostream>
+using namespace std;
+const double PI = 3.1415926;
+class Point {
+private:
+	int x, y;
+public:
+	Point(int t_x = 0, int t_y = 0):
+	x(t_x), y(t_y){}
+	virtual double cal() {
+		return x;
+	}
+};
+class Circle:public Point {
+private:
+	double r;
+public:
+	Circle(int t_x = 0, int t_y = 0, double t_r = 0):
+	r(t_r), Point(t_x, t_y) {}
+	double cal() {
+		return r * r * PI;
+	}
+};
+class Cylinder :public Circle {
+private:
+	double h;
+public:
+	Cylinder(int t_x = 0, int t_y = 0, double t_r = 0, double t_h = 0):
+	h(t_h), Circle(t_x, t_y, t_r){}
+	double cal() {
+		return Circle::cal() * h;
+	}
+};
+int main(){	
+	Point *p;
+	Cylinder a1(1, 2, 3, 4);
+	p = &a1;
+	cout << "体积为：" << p->cal() << endl;
+	Circle a2(1, 2, 3);
+	p = &a2;
+	cout << "面积为：" << p->cal() << endl;
+	Point a3(1, 2);
+	p = &a3;
+	cout << "x = " << p->cal() << endl;
+	system("pause");
+	return 0;
+}
---
+++ C++作业/c++作业11.cpp
@@ -0,0 +1,60 @@
+﻿/*
+*声明一个教师（Teacher）类和一个学生（Student）类，
+*用多重继承的方式声明一个研究生（Graduate）派生类。
+*教师类中包括数据成员name（姓名），age（年龄），title（职称）。
+*学生类中包括数据成员name（姓名），age（年龄），score（成绩）。
+*在定义派生类对象时给出初始化的数据（自已定），然后输出这些数据。
+*初值自拟
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+class Teacher {
+private:
+	string tea_name;
+	int tea_age;
+	string title;
+public:
+	Teacher(string tea_name_, int tea_age_, string title_) :
+	tea_name(tea_name_), tea_age(tea_age_), title(title_){}
+	void tea_show() {
+		cout << "老师姓名：" << tea_name << endl;
+		cout << "老师年龄：" << tea_age << endl;
+		cout << "老师职位：" << title << endl;
+	}
+};
+class Student {
+private:
+	string stu_name;
+	int stu_age;
+	string socre;
+public:
+	Student(string stu_name_, int stu_age_, string socre_):
+	stu_name(stu_name_), stu_age(stu_age_), socre(socre_){}
+	void stu_show() {
+		cout << "学生姓名：" << stu_name << endl;
+		cout << "学生年龄：" << stu_age << endl;
+		cout << "学生成绩：" << socre << endl;
+	}
+};
+class Graduate:public Teacher, public Student {
+private:
+	string major;
+public:
+	Graduate(string major_, string tea_name_, int tea_age_, string title_,
+		string stu_name_, int stu_age_, string socre_) :
+	major(major_), Teacher(tea_name_, tea_age_, title_), 
+	Student(stu_name_, stu_age_, socre_){}
+	void gra_show() {
+		cout << "专业：" << major << endl;
+	}
+};
+int main(){	
+	Graduate gra1("软件", "小明", 44, "院长", "小花", 20, "59");
+	gra1.tea_show();
+	gra1.gra_show();
+	gra1.stu_show();
+	system("pause");
+	return 0;
+}
